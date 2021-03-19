63160978ce634288a233cf0ca03346e89d37df6c
Borislav Damyanov <boris.damyanov@gmail.com>
2021-03-19 10:13:01 +0100

    The Fault Tolerance is functioning

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,12 +4,14 @@ import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients("com.example.userservice")
+@EnableHystrix
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -18,6 +20,7 @@ public class UserServiceApplication {
 	}
 	@Bean
 	public Sampler defaultSampler(){
+
 		return Sampler.ALWAYS_SAMPLE;
 	}
 
--- user-service/src/main/java/com/example/userservice/controllers/UserController.java
+++ user-service/src/main/java/com/example/userservice/controllers/UserController.java
@@ -4,11 +4,13 @@ import com.example.userservice.beans.PostBean;
 import com.example.userservice.entities.User;
 import com.example.userservice.feign_client.PostServiceProxy;
 import com.example.userservice.services.interfaces.IUserService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -46,7 +48,10 @@ public class UserController {
         return userService.getUserById(id).get();
     }
 
+    //Feign Client  Requests
+
     @GetMapping("/feign-proxy-post/{id}")
+    @HystrixCommand(fallbackMethod = "fallbackGetPost")
     public PostBean getPost(@PathVariable Long id) {
         PostBean response = feignPostServiceProxy.getPost(id);
         log.info("{}", response);
@@ -54,7 +59,9 @@ public class UserController {
 
     }
 
+
     @GetMapping("/feign-proxy-posts/{id}")
+    @HystrixCommand(fallbackMethod = "fallbackGetPostsByUserId")
     public List<PostBean> getPostsByUserId(@PathVariable Long id) {
         List<PostBean> response = feignPostServiceProxy.getAllPostByUserId(id);
         log.info("{}", response);
@@ -64,6 +71,18 @@ public class UserController {
         }
         return resultList;
 
+    }
+
+    //Fault Tolerance
+
+    public PostBean fallbackGetPost(@PathVariable Long id) {
+        return new PostBean(10001L, "This is the fallback post message", 2L);
+    }
+
+    public List<PostBean> fallbackGetPostsByUserId(@PathVariable Long id) {
+        List<PostBean> fallbackPostBeanList = Arrays.asList(new PostBean(10001L, "This is the fallback list post message", 2L),
+                new PostBean(10002L, "This is the fallback post list  message", 1L));
+     return fallbackPostBeanList;
     }
 
 
